9bf4fa5a47df230cd3f33c24d8d92808f6a1a155
Md. Salman Rahman <107785615+Salman2118@users.noreply.github.com>
2023-07-25 15:03:19 +0600

    Add files via upload

---
+++ 2nd_con_v4/js/custom.js
@@ -0,0 +1,22 @@
+$(function (){
+
+
+
+
+$('.team-work').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows:false,
+  speed:1500,
+  autoplaySpeed: 2000,
+    });
+
+
+
+
+
+
+
+});
+    
